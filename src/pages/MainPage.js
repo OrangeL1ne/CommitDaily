1352b1f870d0ce573f25b8cf6d07277028322dbc
heesun <heesunmelody@gmail.com>
2022-03-24 00:23:54 +0900

    Fix MainPage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
-import {Temp} from "../components/Temp";
+import { ProgressBar } from "../components/ProgressBar";
+import { TeamInfo } from "../components/TeamInfo";
+import { StatusCard } from "../components/StatusCard";
 
-const PageContainer = styled.div``;
+const PageContainer = styled.div`
+  margin: 100px;
+`;
 
 const MainPage = () => {
   return (
     <PageContainer>
-      <Temp/>
+      <ProgressBar value="70"/>
+      <TeamInfo content="md파일!!"/>
+      <StatusCard title="오늘의 출석률" current="2" total="/7명"/>
+      <StatusCard title="전체 출석 일수" current="2" total="/8일"/>
+      <StatusCard title="전체 커밋 개수" current="47개"/>
     </PageContainer>
-  )
-}
+  );
+};
 
 export default MainPage;
