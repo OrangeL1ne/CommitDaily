213524def985f216645abdcda8f7e9a3a0140161
Seunghee Lee <49032882+seunghee9941@users.noreply.github.com>
2022-03-30 21:08:29 +0900

    feat: add Header

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import styled from "styled-components";
-import {Temp} from "../components/Temp";
+import {Header} from "../components/Header";
 
-const PageContainer = styled.div``;
+const PageContainer = styled.main`
+  max-width: 1194px;
+  margin: 0 auto;
+  padding: 121px 0;
+`;
 
 const MainPage = () => {
+
+  function handleLogin() {
+    // TODO: login
+  }
+
   return (
-    <PageContainer>
-      <Temp/>
-    </PageContainer>
-  )
+    <>
+      <Header _onClick={handleLogin} />
+      <PageContainer>
+      </PageContainer>
+    </>
+  );
 }
 
 export default MainPage;
