42dfa51b167afb2e9ec42be03837c8c37b52ce7f
Seunghee Lee <49032882+seunghee9941@users.noreply.github.com>
2022-04-13 18:33:25 +0900

    feat: add CommitCard

--- src/assets/TeamData.js
+++ src/assets/TeamData.js
@@ -1,5 +1,5 @@
 export const TeamData = {
   team: 'TEAM NAME 🍀',
   start: new Date('2022-01-01'),
-  end: new Date('2022-02-02'),
+  end: new Date('2022-01-17')
 };
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,12 +2,18 @@ import React from 'react'
 import styled from "styled-components";
 import {Header} from "../components/Header";
 import {TeamCard} from "../components/TeamCard";
+import {CommitCard} from "../components/CommitCard";
 import {TeamData} from "../assets/TeamData";
+import {TempUser} from "../assets/TempUser";
 
 const PageContainer = styled.main`
   max-width: 1194px;
   margin: 0 auto;
   padding: 121px 0;
+`;
+
+const Section = styled.section`
+  margin: ${props => props.top || 0}px ${props => props.right || 0}px 0 0;
 `;
 
 const MainPage = () => {
@@ -21,6 +27,9 @@ const MainPage = () => {
       <Header _onClick={handleLogin} />
       <PageContainer>
         <TeamCard data={TeamData} />
+        <Section top={48}>
+          <CommitCard data={TempUser} />
+        </Section>
       </PageContainer>
     </>
   );
